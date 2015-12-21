b9867dfd8e070fed75a2055c5e9e1effc2fde2dd
laplly <176469524@qq.com>
2015-12-21 18:31:19 +0800

    Git!!!!

---
+++ gituser/gituser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gituser
+//
+//  Created by 李安培 on 15/12/21.
+//  Copyright (c) 2015年 李安培. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gituser/gituser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gituser
+//
+//  Created by 李安培 on 15/12/21.
+//  Copyright (c) 2015年 李安培. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
